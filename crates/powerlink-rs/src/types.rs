ee95f7120cf49f8dc6d1696822047ada2ac42f90
Fabio Thomaz Molinar <fabiomolinar@gmail.com>
2025-09-25 19:23:31 +0200

    fixed lint warnings.

--- crates/powerlink-rs/src/types.rs
+++ crates/powerlink-rs/src/types.rs
@@ -26,8 +26,10 @@ pub type REAL32 = f32;
 /// Alias for REAL64 (64-bit floating point).
 pub type REAL64 = f64;
 /// Alias for a 6-byte MAC Address.
+#[allow(non_camel_case_types)]
 pub type MAC_ADDRESS = [u8; 6];
 /// Alias for a 4-byte IP Address.
+#[allow(non_camel_case_types)]
 pub type IP_ADDRESS = [u8; 4];
 
 
