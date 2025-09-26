35d58c342c3327b0df76882b4b054f2fc40fec1b
Fabio Thomaz Molinar <fabiomolinar@gmail.com>
2025-09-26 15:14:17 +0200

    Updated DLL_MS.

--- crates/powerlink-rs/src/frame/ms_state_machine.rs
+++ crates/powerlink-rs/src/frame/ms_state_machine.rs
@@ -50,9 +50,52 @@ impl DllMsStateMachine {
 
     /// Processes an incoming event and transitions the state based on the current NMT state.
     /// The logic follows the state diagram in Figure 30 of the specification.
-    pub fn process_event(&mut self, event: DllMsEvent, nmt_state: NMTState) {
+    pub fn process_event(
+        &mut self, event: DllMsEvent, nmt_state: NMTState, response_expected: bool, 
+        async_in: bool, async_out: bool, isochr: bool
+    ) {
         // The DLL_CS is active only in specific NMT states.
-        
+        match nmt_state {
+            NMTState::NMT_MS_PRE_OPERATIONAL_1 => {
+                let next_state = match (self.state, event) {
+                    (DllMsState::DLL_MS_WAIT_SOA, DllMsEvent::DLL_ME_SOA_TRIG) => {
+                        match (async_in, response_expected, async_out) {
+                            // --- (DLL_MT10) ---
+                            // Send SoA with Invite
+                            (false, false, _) => DllMsState::DLL_MS_WAIT_SOA,
+                            // Send SoA with Invite to MN and send ASnd or non POWERLINK frame
+                            (true, _, false) => DllMsState::DLL_MS_WAIT_SOA,
+                            // Send SoA
+                            (true, _, true) => DllMsState::DLL_MS_WAIT_ASND,
+                            // --- (DLL_MT11) ---
+                            // Send SoA with Invite
+                            (false, true, _) => DllMsState::DLL_MS_WAIT_ASND,
+                        }
+                    },                    
+                    (DllMsState::DLL_MS_WAIT_ASND, DllMsEvent::DLL_ME_ASND_TIMEOUT | DllMsEvent::DLL_ME_SOA_TRIG) => {
+                        match (async_in, response_expected) {
+                            // --- (DLL_MT12) ---
+                            (true, _) => DllMsState::DLL_MS_WAIT_ASND,
+                            (false, false) => DllMsState::DLL_MS_WAIT_SOA,
+                            // --- (DLL_MT13) ---
+                            (false, true) => DllMsState::DLL_MS_WAIT_SOA,
+                        }
+                    },
+                    // If an unexpected event occurs, remain in the current state.
+                    // Error reporting would be triggered here.
+                    (current, _) => current,                    
+                };
+                self.state = next_state;
+            },
+            NMTState::NMT_MS_OPERATIONAL | NMTState::NMT_MS_READY_TO_OPERATE | NMTState::NMT_MS_PRE_OPERATIONAL_2 => {
+                // TODO
+
+            },
+            _ => {
+                // In all other NMT states, the Dll state machine is considered non-cyclic.
+                self.state = DllMsState::DLL_MS_NON_CYCLIC;
+            }
+        }
     }
 
     /// Returns the current state of the DLL state machine.
@@ -63,7 +106,7 @@ impl DllMsStateMachine {
 
 impl Default for DllMsStateMachine {
     fn default() -> Self {
-        Self { state: DllMsState::DLL_CS_NON_CYCLIC }
+        Self { state: DllMsState::DLL_MS_NON_CYCLIC }
     }
 }
 
